b190b1c93ab3ffeb0512462f39abc4618ef80175
Muhammed Hattap Alkan <180018483+antihume@users.noreply.github.com>
2026-02-21 11:11:52 +0300

    emacs: add delay to embark's which-key popup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -564,24 +564,34 @@ Only activates mappings for languages with installed grammars."
   :ensure t
   :preface
   (defun my--embark-which-key-indicator ()
-    "An embark indicator that displays keymaps using which-key."
-    (lambda (&optional keymap targets prefix)
-      (if (null keymap)
-          (which-key--hide-popup-ignore-command)
-        (which-key--show-keymap
-         (if (eq (plist-get (car targets) :type) 'embark-become)
-             "Become"
-           (format "Act on %s '%s'%s"
-                   (plist-get (car targets) :type)
-                   (embark--truncate-target (plist-get (car targets) :target))
-                   (if (cdr targets) "…" "")))
-         (if prefix
-             (pcase (lookup-key keymap prefix 'accept-default)
-               ((and (pred keymapp) km) km)
-               (_ (key-binding prefix 'accept-default)))
-           keymap)
-         nil nil t (lambda (binding)
-                     (not (string-suffix-p "-argument" (cdr binding))))))))
+    "An embark indicator that displays keymaps using which-key.
+The which-key popup is delayed by `which-key-idle-delay'."
+    (let ((timer nil))
+      (lambda (&optional keymap targets prefix)
+	(if (null keymap)
+            (progn
+              (when timer (cancel-timer timer) (setq timer nil))
+              (which-key--hide-popup-ignore-command))
+          (when timer (cancel-timer timer))
+          (setq timer
+		(run-with-idle-timer
+		 which-key-idle-delay nil
+		 (lambda ()
+                   (setq timer nil)
+                   (which-key--show-keymap
+                    (if (eq (plist-get (car targets) :type) 'embark-become)
+			"Become"
+                      (format "Act on %s '%s'%s"
+                              (plist-get (car targets) :type)
+                              (embark--truncate-target (plist-get (car targets) :target))
+                              (if (cdr targets) " " "")))
+                    (if prefix
+			(pcase (lookup-key keymap prefix 'accept-default)
+                          ((and (pred keymapp) km) km)
+                          (_ (key-binding prefix 'accept-default)))
+                      keymap)
+                    nil nil t (lambda (binding)
+				(not (string-suffix-p "-argument" (cdr binding))))))))))))
   (defun my--embark-hide-which-key-indicator (fn &rest args)
     "Hide the which-key indicator when using the completing-read prompter."
     (which-key--hide-popup-ignore-command)
